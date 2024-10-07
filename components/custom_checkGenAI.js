687c77309049714d86ff78fa8d53b24f2ad4a3c2
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-10-07 22:40:32 +0530

    update

--- components/custom_checkGenAI.js
+++ components/custom_checkGenAI.js
@@ -5,39 +5,45 @@ const conversationHistory = require('../utils/conversationHistory');
 module.exports = {
 	metadata: {
 		name: 'CUSTOM_Check_GenAi',
-		properties: {},
+		properties: {
+			userMessage: {
+				type: 'string',
+				description: 'User message',
+			},
+		},
 		supportedActions: [],
 	},
 	invoke: async (context, done) => {
-		// const authenticationProvider = await new common.InstancePrincipalsAuthenticationDetailsProviderBuilder().build();
-
 		const logger = context.logger();
 		let History = context.variable('user.conversationHistory');
-		const authenticationProvider =
-			await common.ResourcePrincipalAuthenticationDetailsProvider.builder();
 
-		// const configurationFilePath = '~/.oci_dmcc/config';
-		// const configProfile = 'DEFAULT';
+		try {
+			const authenticationProvider =
+				await common.ResourcePrincipalAuthenticationDetailsProvider.builder().build();
 
-		// const authenticationProvider =
-		// 	new common.ConfigFileAuthenticationDetailsProvider(
-		// 		configurationFilePath,
-		// 		configProfile
-		// 	);
-		const client = new genai.GenerativeAiInferenceClient({
-			authenticationDetailsProvider: authenticationProvider,
-		});
+			const userMessage = context.getUserMessage();
+			// const configurationFilePath = '~/.oci_dmcc/config';
+			// const configProfile = 'DEFAULT';
 
-		const compartmentId =
-			'ocid1.tenancy.oc1..aaaaaaaahqvb2kliqi35z57qalhpr4dyqbjprclszdcoar2wgc7q6nl36aba';
-		// const compartmentId =
-		// 	'ocid1.compartment.oc1..aaaaaaaacdv3ig7yho6ebvdm6p5cdxq74pgnugeankt25mebpxoyhar2n4pa';
+			// const authenticationProvider =
+			// 	new common.ConfigFileAuthenticationDetailsProvider(
+			// 		configurationFilePath,
+			// 		configProfile
+			// 	);
+			const client = new genai.GenerativeAiInferenceClient({
+				authenticationDetailsProvider: authenticationProvider,
+			});
 
-		if (History === null || History === undefined) {
-			conversationHistory.storeConversationHistory(
-				context,
-				'SYSTEM',
-				`## Instructions
+			const compartmentId =
+				'ocid1.tenancy.oc1..aaaaaaaahqvb2kliqi35z57qalhpr4dyqbjprclszdcoar2wgc7q6nl36aba';
+			// const compartmentId =
+			// 	'ocid1.compartment.oc1..aaaaaaaacdv3ig7yho6ebvdm6p5cdxq74pgnugeankt25mebpxoyhar2n4pa';
+
+			if (History === null || History === undefined) {
+				conversationHistory.storeConversationHistory(
+					context,
+					'SYSTEM',
+					`## Instructions
 			You are an intelligent routing agent designed to assist employees in booking time off work. Your primary goal is to determine the appropriate Absence Plan for each request and guide the conversation to gather all necessary information. If the user is not asking for a leave, you have to respond the user in a way that they will ask for a leave. Follow these steps:
 
 			1. Analyze the conversation log and the user's latest query.
@@ -59,90 +65,96 @@ module.exports = {
 				"questionToAsk": string | null,
 				"explanation": string
 			}`.replace(/\t/g, '')
-			);
-		}
+				);
+			}
 
-		const userMessage = context.getUserMessage();
-		const preambleOverride = `You are an intelligent routing agent designed to assist employees in booking time off work. You have to provide your response always in json Format. Follow the given instructions properly`;
-
-		conversationHistory.storeConversationHistory(
-			context,
-			'USER',
-			userMessage.text
-		);
-
-		try {
-			const modelId = 'cohere.command-r-plus';
-			// const modelId = 'ocid1.generativeaimodel.oc1.eu-frankfurt-1.amaaaaaask7dceyaazssnpqc7g4rxwlvcfehpcfbtdvvkftz3jzz5pf4tenq';
-			const prompt = userMessage;
-			const conversationHistory = context.variable('user.conversationHistory');
-			const max_tokens = 1800;
-			const temperature = 0;
-			const top_k = 0.7;
-			const docs = [
-				{
-					title: 'Sick Leave',
-					snippet:
-						'Used if the employee is unable to attend work due to a medical reason, such as they are ill, or have to attend a medical appointment',
-				},
-				{
-					title: 'Annual Leave',
-					snippet:
-						'The employee is entitled to take 20 days annual leave at their discretion without giving a particular reason. The purpose is simply to give them a break from work. This time might also be referred to as vacation, Personal Time Off (PTO), or annual leave.',
-				},
-				{
-					title: 'Remote Working',
-					snippet:
-						'Used if the employee wishes to have some time working outside of the office - for example working from home, or another remote location.',
-				},
-			];
-			const serving_mode = {
-				modelId: modelId,
-				servingType: 'ON_DEMAND',
-			};
-
-			const inference_request = {
-				message: prompt,
-				apiFormat: 'COHERE',
-				citationQuality: 'ACCURATE',
-				maxTokens: max_tokens,
-				preambleOverride: preambleOverride,
-				temperature: temperature,
-				documents: docs,
-				chatHistory: conversationHistory,
-				topK: top_k,
-			};
-
-			const chatDetails = {
-				compartmentId: compartmentId,
-				servingMode: serving_mode,
-				chatRequest: inference_request,
-			};
-
-			logger.info('CUSTOM_Check_GenAi: ', chatDetails);
-			const chatResponse = await client.chat({ chatDetails: chatDetails });
+			const preambleOverride = `You are an intelligent routing agent designed to assist employees in booking time off work. You have to provide your response always in json Format. Follow the given instructions properly`;
 
 			conversationHistory.storeConversationHistory(
 				context,
-				'CHATBOT',
-				chatResponse.chatResult.chatResponse.text
+				'USER',
+				userMessage.text
 			);
 
-			let response = JSON.parse(
-				chatResponse.chatResult.chatResponse.text
-					.replace(/`/g, '')
-					.replace('json', '')
-					.trim()
-			);
-			if (response.hasEnoughInfo && !response.needMoreInfo) {
-				context.reply(response.selectedPlan);
-			} else {
-				context.reply(response.questionToAsk);
+			try {
+				const modelId = 'cohere.command-r-plus';
+				// const modelId = 'ocid1.generativeaimodel.oc1.eu-frankfurt-1.amaaaaaask7dceyaazssnpqc7g4rxwlvcfehpcfbtdvvkftz3jzz5pf4tenq';
+				const prompt = userMessage;
+				const conversationHistory = context.variable(
+					'user.conversationHistory'
+				);
+				const max_tokens = 1800;
+				const temperature = 0;
+				const top_k = 0.7;
+				const docs = [
+					{
+						title: 'Sick Leave',
+						snippet:
+							'Used if the employee is unable to attend work due to a medical reason, such as they are ill, or have to attend a medical appointment',
+					},
+					{
+						title: 'Annual Leave',
+						snippet:
+							'The employee is entitled to take 20 days annual leave at their discretion without giving a particular reason. The purpose is simply to give them a break from work. This time might also be referred to as vacation, Personal Time Off (PTO), or annual leave.',
+					},
+					{
+						title: 'Remote Working',
+						snippet:
+							'Used if the employee wishes to have some time working outside of the office - for example working from home, or another remote location.',
+					},
+				];
+				const serving_mode = {
+					modelId: modelId,
+					servingType: 'ON_DEMAND',
+				};
+
+				const inference_request = {
+					message: prompt,
+					apiFormat: 'COHERE',
+					citationQuality: 'ACCURATE',
+					maxTokens: max_tokens,
+					preambleOverride: preambleOverride,
+					temperature: temperature,
+					documents: docs,
+					chatHistory: conversationHistory,
+					topK: top_k,
+				};
+
+				const chatDetails = {
+					compartmentId: compartmentId,
+					servingMode: serving_mode,
+					chatRequest: inference_request,
+				};
+
+				logger.info('CUSTOM_Check_GenAi: ', chatDetails);
+				const chatResponse = await client.chat({ chatDetails: chatDetails });
+
+				conversationHistory.storeConversationHistory(
+					context,
+					'CHATBOT',
+					chatResponse.chatResult.chatResponse.text
+				);
+
+				let response = JSON.parse(
+					chatResponse.chatResult.chatResponse.text
+						.replace(/`/g, '')
+						.replace('json', '')
+						.trim()
+				);
+				if (response.hasEnoughInfo && !response.needMoreInfo) {
+					context.reply(response.selectedPlan);
+				} else {
+					context.reply(response.questionToAsk);
+				}
+			} catch (error) {
+				logger.error('chat Failed with error  ' + error);
 			}
 		} catch (error) {
-			logger.error('chat Failed with error  ' + error);
+			logger.error('Error in CUSTOM_Check_GenAi: ' + error);
+			context.reply('Sorry, there was an error processing your request.');
+		} finally {
+			context.transition();
+			done();
 		}
-		context.transition();
-		done();
 	},
 };
